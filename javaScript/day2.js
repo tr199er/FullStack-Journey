f4fc0c9d541ece251103d238b43c7da69a5ad93d
akanni malik <64205553+tr199er@users.noreply.github.com>
2025-05-21 14:05:48 +0100

    hcdjc
    javascript

--- javaScript/day2.js
+++ javaScript/day2.js
@@ -1,38 +1,13 @@
-//JAVASCRIPT OBJECT AND THE DOM
+const myName = "Akanni Malik";
+let myAge = 25;
+let isStudent = true;
 
-//OBJECT
-const person = {
-  name: "Akanni Malik",
-  age: 25,
-  isCoder: true,
-  bestFoods: ["beans", "cake", "plantain"],
-};
+console.log("my name is " + myName);
+console.log("I am " + myAge + " years old.");
+console.log("Am I a student? " + isStudent);
+console.log("next year i will be " + (myAge + 1));
 
-console.log(person.age);
-console.log(person["isCoder"]);
-
-person.job = "developer";
-person.age = 24;
-
-console.log(person);
-
-const calculator = {
-  add: (a, b) => a + b,
-};
-
-console.log(calculator.add(2, 4));
-
-//THE DOM
-const data = {
-  items: ["apple", "Orange", "Grape"],
-};
-const ul = document.querySelector("#casing");
-ul.innerHTML = data.items.map((data) => `<li>${data}</li>`).join("");
-
-console.log(ul);
-
-//EXERCISE 1
-function countChars(str) {
-  const letterCount = {};
-}
-console.log(countChars("aabbc")); // {a: 2, b: 2, c: 1}
+// Using template literals
+console.log(`My name is ${myName}.`);
+console.log(`I am ${myAge} years old.`);
+console.log(`Am I a student? ${isStudent}.`);
---
+++ javaScript/day3.js
@@ -0,0 +1,16 @@
+const num1 = 7;
+const num2 = 5;
+let isPositive;
+
+console.log(`Sum: ${num1 + num2}`);
+console.log(`Reminder: ${num1 % num2}`);
+console.log(`Is num1 greater then num2: ${num1 > num2}`);
+
+isPositive = num1 > 0 && num2 > 0;
+
+console.log(`Are both numbers positive?: ${isPositive}`);
+
+//It was challenging at first but with being calm i was able to scale through
+//And the operators are funny that they i feel i need proper understanding of the operators so i don't have a problem with it on the long run, so i took my time on it
+
+//Doing day 3 today so i would start day 4 on tuesday
